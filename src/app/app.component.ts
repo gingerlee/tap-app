67aae593a77c54ef9d31a143e24ea5d1094753a7
Christopher Knight Johnson and Ginger Lee Kretschmer <christopher@>
2018-01-17 16:25:40 -0800

    refractor code to include adding classes to different prices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,50 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Ginga\'s tap room';
   kegs: Keg[] = [
-    new Keg('GingaNinja', 'Two Towns', '$5.50', '5.2%'),
-    new Keg('Nights of Chris-Lumbus', 'Geno-Ciders', '$6.50', '7.5%'),
-    new Keg('Over Hopulation', 'Barley Brown\'s Beer', '$6.00', '8.5%'),
-    new Keg('Black (黑)', 'Mikkeller', '$9.00', '17.5%')
+    new Keg('GingaNinja', 'Two Towns', 3.50, '5.2%'),
+    new Keg('Nights of Chris-Lumbus', 'Geno-Ciders', 6.50, '7.5%'),
+    new Keg('Over Hopulation', 'Barley Brown\'s Beer', 6.00, '8.5%'),
+    new Keg('Black (黑)', 'Mikkeller', 9.00, '17.5%')
   ];
-}
-export class Keg {
-  public pintsLeft: number = 120;
-
-  constructor(public name: string, public brand: string, public price: string, public alcoholContent: string) {
-    // this.description: string = `${this.name}, by ${this.brand}, ${this.price}, ${this.alcoholContent}`
+  selectedKeg: Keg = this.kegs[0];
+  selectKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
   }
   hookUp(name, brand, price, alcoholContent){
-    
+    this.kegs.push(new Keg(name, brand, price, alcoholContent));
   }
+}
+class Keg {
+  public pintsLeft: number = 15;
+
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: string) { }
+  purchasePint() {
+    if (this.pintsLeft > 0) {
+      this.pintsLeft--;
+    }
+  }
+
+  priceColor(){
+    if (this.price < 5){
+      return 'price-low';
+    } else if (this.price < 8 ) {
+      return 'price-medium';
+    } else {
+      return 'price-high';
+    }
+  }
+  // isPriceLow() {
+  //   if (parseInt(this.price.replace(/[^\d]/g, ''), 10) <= 500){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+  // isPriceMedium() {
+  //   if (parseInt(this.price.replace(/[^\d]/g, ''), 10) <= 750){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
 }
