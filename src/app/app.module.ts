e63e5f3f18702d97bd6c66c6f8cdcef6b2a8fc3c
Christopher Knight Johnson and Ginger Lee Kretschmer <Guest@Epicodus-4B.local>
2018-01-18 10:25:01 -0800

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule }  from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NgClass } from '@angular/common';
+import { KegListComponent } from './keg-list/keg-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KegListComponent
   ],
   imports: [
     BrowserModule, FormsModule
